bcbb37d3bd3238eba8f2ff6fc71de562dea35a9b
dxsilez <t2bb94@gmail.com>
2020-03-14 03:38:32 +0300

    updated logic of message shown

--- hello_world.py
+++ hello_world.py
@@ -1,2 +1,8 @@
-text = 'Hello world'
-print(text)
+while (True):
+    text = input()
+    if (len(text) < 5):
+        print('message is too short for me')
+    else:
+        print('Thanks! text is: ', text)
+        print('Bye!')
+        break

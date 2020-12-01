7f2cdbd73ad6593726e9b3369293a31125aba0d0
dxsilez <t2bb94@gmail.com>
2020-12-02 00:25:31 +0300

    занятие_занятие

--- hello_world.py
+++ hello_world.py
@@ -1,8 +1,26 @@
-while (True):
-    text = input()
-    if (len(text) < 5):
-        print('message is too short for me')
-    else:
-        print('Thanks! text is: ', text)
-        print('Bye!')
-        break
+# while (True):
+#     text = input()
+#     if (len(text) < 5):
+#         print('message is too short for me')
+#     else:
+#         print('Thanks! text is: ', text)
+#         print('Bye!')
+#         break
+
+students = ['23','654','65']
+students.append('ghja')
+students+=['hjklsty']
+students+=('prte')
+students.insert(2, 'PORTER')
+students.remove('23')
+del students[2]
+for elem in students:
+    print(elem + ', length ' + str(len(elem)))
+for elem in students:
+    print(elem)
+if '65' in students:
+    print('65 is in students.list')
+    print(students.index('PORTER'))
+sorted_students = sorted(students)
+print(sorted_students)
+
